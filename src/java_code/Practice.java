0c4e2ce36ea7cd17de376cc5a8f3957e9ef78f01
tanweerbuddy@gmail.com <tanweerbuddy@gmail.com>
2023-04-08 17:38:01 +0530

    lolu

--- src/java_code/Practice.java
+++ src/java_code/Practice.java
@@ -5,6 +5,7 @@ public class Practice
 	public static void main(String[] args) {
 		System.out.println("Tanu");
 		System.out.println("This is Tanvii");
+		System.out.println("This is Shyaam");
 	}
 
 }
